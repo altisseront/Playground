be411dac476494ff3c84a18df4937680b0b8cde0
altisseront <altisseront@gmail.com>
2022-04-06 16:19:24 -0400

    init

---
+++ server.py
@@ -0,0 +1,69 @@
+from turtle import color
+from flask import Flask, render_template  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def index():
+    return render_template("index.html")
+
+@app.route('/play')
+def play():
+    return render_template("play.html", x=3)
+
+@app.route('/play/<int:x>')
+def play1(x):
+    return render_template("play.html", x=x)
+
+@app.route('/play/<int:x>/<y>')
+def play2(x, y):
+    return render_template("play.html", x=x, y=y)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
